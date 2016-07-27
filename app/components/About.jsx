4aea243cf2a0013d1a04c17796244087f26fd188
Michael Mullen <michaeldmullen@gmail.com>
2016-07-27 08:47:12 -0700

    css styles added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var About = (props) => {
 	return (
 		<div>
-			<h1 className="text-center">About</h1>
+			<h1 className="text-center page-title">About</h1>
 			<ul>
 				<li>This app was built with <a href='https://facebook.github.io/react/'>React.js</a></li>
 				<li>Weather data comes from <a href="http://openweathermap.org/">Open Weather Map</a></li>
