99d6aeb53ba1dfa522f937566d03b9ab4c556d8f
Michael Mullen <michaeldmullen@gmail.com>
2016-07-27 11:13:33 -0700

    wired up search box

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -16,7 +16,10 @@ var Weather = React.createClass({
 		
 		this.setState({
 			isLoading: true,
-			errorMessage: undefined
+			errorMessage: undefined,
+			location: undefined,
+			temp: undefined
+
 
 		});
 
@@ -32,6 +35,23 @@ var Weather = React.createClass({
 				errorMessage: e.message
 			});
 		});
+	},
+
+	componentDidMount: function () {
+		var location = this.props.location.query.location;
+
+		if (location && location.length > 0) {
+			this.handleSearch(location);
+			window.location.hash = '#/'
+		}
+	},
+	componentWillReceiveProps: function(newProps) {
+	    var location = newProps.location.query.location;
+
+		if (location && location.length > 0) {
+			this.handleSearch(location);
+			window.location.hash = '#/'
+		}
 	},
 
 	render: function() {
